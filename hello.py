fbd86c6913a875e7608be4c3f691ae7883055559
Adrian Tully <adriantully@hotmail.co.uk>
2024-05-29 21:11:06 +0100

    stepone

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-input("what is your name?")
-print("Hello, world!")
+# ask user for their name
+name = input("what's your name? ")
+
+# remove shitespace from name
+name=name.strip()
+
+# captilise
+name = name.title()
+
+# say hello to user
+print("Hello,", name)
