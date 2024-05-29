d00292c7b22e54c287c42d403ccb61028e830275
Adrian Tully <adriantully@hotmail.co.uk>
2024-05-29 21:21:59 +0100

    moving on

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 # ask user for their name
 name = input("what's your name? ")
+# name = input("what's your name? ").strip().title()
 
 # remove shitespace from name
 name=name.strip()
+
+# name=name.strip().title()
+# allows you to string functions together
 
 # captilise
 name = name.title()
