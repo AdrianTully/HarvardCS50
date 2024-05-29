3afb10537c78dbe879b3312ee4d5e900c78e1844
Adrian Tully <adriantully@hotmail.co.uk>
2024-05-29 22:03:34 +0100

    module1

--- calculator.py
+++ calculator.py
@@ -1,4 +1,8 @@
-x=int(input("What's X? "))
-y=int(input("What's Y? "))
+x=float(input("What's X? "))
+y=float(input("What's Y? "))
 
-print(x+y)
+# z=round(x+y)
+# z=x/y
+z=round(x/y, 2)
+
+int(f"{z:,}")
